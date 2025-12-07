6977a07be44b8455cfad78cd1461daaa5797966b
gibhub action <action@github.com>
2025-12-07 11:15:28 +0100

    update release

--- src/string.ts
+++
@@ -1,5 +0,0 @@
-export default class String {
-  static isNullOrEmpty(value: string | unknown[] | undefined): boolean {
-    return !value || value === undefined || value === "" || value.length === 0;
-  }
-}
